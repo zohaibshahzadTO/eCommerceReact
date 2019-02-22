9b12ad8b922e6aaf42c57d5e6e9770e26ce82cbd
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-02-22 00:41:46 -0500

    Updates
    - Completed ProductList.js component
    - Instead of prop drilling, we're utilizing context api
    - Extensive work done on Product.js component

--- my-app/src/components/ProductList.js
+++ my-app/src/components/ProductList.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from './Title';
-import { storeProducts } from '../data';
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  }
   render() {
     return (
       <React.Fragment>
@@ -14,7 +11,13 @@ export default class ProductList extends Component {
           <div className="container">
             <Title name="our" title="products" />
             <div className="row">
- 
+            <ProductConsumer>
+              {value => {
+                return value.products.map( product => {
+                  return <Product key={product.id} product={product} />;
+                });
+              }}
+            </ProductConsumer>
             </div>
           </div>
         </div>
